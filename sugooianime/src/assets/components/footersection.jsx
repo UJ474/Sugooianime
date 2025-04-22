8c131992977ede28c775857bf9c93a178a9e929b
Utkarsh Jain <utkarshj479@gmail.com>
2025-04-23 00:30:34 +0530

    header done

---
+++ sugooianime/src/assets/components/footersection.jsx
@@ -0,0 +1,11 @@
+import '../css_files/footersection.css'
+
+export default function Footer() {
+    return (
+        <div className="footersectionmain">
+        <p>© 2025 Sugooianime. All rights reserved.</p>
+
+        
+        </div>
+    )
+}
---
+++ sugooianime/src/assets/components/headersection.jsx
@@ -0,0 +1,37 @@
+import '../css_files/headersection.css'
+import searchImage from '../images/searchimage.png';
+import savedImage from '../images/savedimage.png';
+import accountImage from '../images/accountimage.png';
+import hamburgerimage from '../images/hamburgerimage.png';
+
+export default function Header() {
+    const headertextlinksleft = [
+        {heading: 'Home',},
+        {heading: 'New',},
+        {heading: 'Most Popular',},
+        {heading: 'Genre',},
+    ]
+
+    const headertextlinksright = [
+        { image: searchImage, alt: 'Search' },
+        { image: savedImage, alt: 'Saved' },
+        { image: accountImage, alt: 'Account' },
+    ];
+    
+    return (
+        <nav className="headersectionmain">
+            <div className="navbarleft-container">
+                <img src={hamburgerimage} alt='hamburgerimage' className='hamburgerimage'/>
+                {headertextlinksleft.map((heading, index) => {
+                    return <div key={index} className='navbarleft hamburger'>{heading.heading}</div>;
+                })}
+            </div>
+            <div className="navbarright-container">
+                {headertextlinksright.map((Header, index) => {
+                    return <img key={index} src={Header.image} alt={Header.alt} className='navbarright' />;
+                })}
+            </div>
+        </nav>
+        
+    )
+}
