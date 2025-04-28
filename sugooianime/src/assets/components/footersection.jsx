da1147bf470b7dbb8432bcd1dc4a2236312ca09b
Utkarsh Jain <utkarshj479@gmail.com>
2025-04-28 22:22:56 +0530

    editted on 28th

--- sugooianime/src/assets/components/footersection.jsx
+++ sugooianime/src/assets/components/footersection.jsx
@@ -4,8 +4,6 @@ export default function Footer() {
     return (
         <div className="footersectionmain">
         <p>© 2025 Sugooianime. All rights reserved.</p>
-
-        
         </div>
     )
 }
